7dd8329884973f4bd21c09ae19c58fda8ebdbb4f
Abu Desai <abhyuday.desai@gmail.com>
2023-01-02 14:35:23 -0800

    cleanup; model_config cleanup; remove infer_file; reorganize explanations if XAI; .txt to failure files

--- app/algorithm/model/mc_classifier.py
+++ app/algorithm/model/mc_classifier.py
@@ -21,8 +21,7 @@ class Classifier():
         self.weights = weights
         self.p = int(p)
         self.algorithm = algorithm
-        self.leaf_size = int(leaf_size)
-        
+        self.leaf_size = int(leaf_size)        
         
         self.model = self.build_model()     
         
@@ -36,14 +35,12 @@ class Classifier():
         self.model.fit(train_X, train_y)            
         
     
-    def predict(self, X, verbose=False): 
-        preds = self.model.predict(X)
-        return preds 
+    def predict(self, X): 
+        return self.model.predict(X) 
     
     
-    def predict_proba(self, X, verbose=False): 
-        preds = self.model.predict_proba(X)
-        return preds 
+    def predict_proba(self, X): 
+        return self.model.predict_proba(X) 
     
 
     def summary(self):
@@ -63,23 +60,15 @@ class Classifier():
 
     @classmethod
     def load(cls, model_path):         
-        knnclassifier = joblib.load(os.path.join(model_path, model_fname))
-        # print("where the load function is getting the model from: "+ os.path.join(model_path, model_fname))        
-        return knnclassifier
+        model = joblib.load(os.path.join(model_path, model_fname))
+        return model
 
 
 def save_model(model, model_path):
-    # print(os.path.join(model_path, model_fname))
-    joblib.dump(model, os.path.join(model_path, model_fname)) #this one works
-    # print("where the save_model function is saving the model to: " + os.path.join(model_path, model_fname))
-    
+    model.save(model_path)
 
 def load_model(model_path): 
-    try: 
-        model = joblib.load(os.path.join(model_path, model_fname))   
-    except: 
-        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
-            Do you have the right trained model in path: {model_path}?''')
+    model = joblib.load(os.path.join(model_path, model_fname))   
     return model
 
 
